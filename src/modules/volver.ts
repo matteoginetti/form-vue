f18e810c3b6bbbe314461c60b1535ba6a6e2563c
matteo.ginetti <>
2023-05-24 09:21:45 +0200

    Add ui-vue-starter template

---
+++ src/modules/volver.ts
@@ -0,0 +1,22 @@
+import { VolverPlugin } from '@volverjs/ui-vue'
+import { VvButton } from '@volverjs/ui-vue/components'
+import icons from '@volverjs/ui-vue/icons'
+import { UserModule } from '~/types'
+
+// Setup Volver: https://volver.github.io/ui-vue/
+export const install: UserModule = ({ app }) => {
+	app.use(VolverPlugin, {
+		// Add your own icons
+		iconsCollections: icons,
+		// Add your own components through aliases
+		aliases: {
+			MyActionQuiet: VvButton,
+		},
+		// Set default props for your aliases
+		defaults: {
+			MyActionQuiet: {
+				modifiers: 'action-quiet',
+			},
+		},
+	})
+}
